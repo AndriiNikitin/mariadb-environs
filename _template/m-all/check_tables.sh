1ba329ca4aede3b876c092b19c6d8878849b2f06
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-04-24 14:14:37 +0200

    check_tables.sh implemented

--- _template/m-all/check_tables.sh
+++ _template/m-all/check_tables.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+schema=$1
+
+if ! [ -d "__datadir/$schema" ] ; then
+  >&2 echo "Cannot find schema ($schema)"
+  (exit 1)
+else
+  for t in $(__workdir/sql.sh "show tables from $schema") ; do
+    __workdir/sql.sh "check table $schema.$t extended"
+  done
+fi
 :
