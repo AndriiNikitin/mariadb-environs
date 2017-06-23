66f2cf5f1b9152aaa98ae46f45947a1f18510fff
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-06-23 10:46:32 +0200

    fix

--- build_or_download.sh
+++ build_or_download.sh
@@ -2,7 +2,7 @@
 set -e
 [ -z "$1" ] && exit 1
 
-if [ "$(find . -maxdepth 1 -type d -name "m$wid*" | wc -l)" -eq 1 ] ; then
+if [ "$(find . -maxdepth 1 -type d -name "m$wid*" | wc -l)" -ne 1 ] ; then
   >&2 echo "one and only one folder must exist with prefix $1"
   exit 2
 fi
@@ -11,7 +11,7 @@ fi
 if [ -f $1*/download.sh ] ; then
   $1*/download.sh
 # cmake/build only if no build folder exists
-elif ! ls $1*/build 2&>/dev/null
+elif ! ls $1*/build 2&>/dev/null ; then
   $1*/checkout.sh
   $1*/cmake.sh
   $1*/build.sh
