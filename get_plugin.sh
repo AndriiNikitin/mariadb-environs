83b436b8b51dc9fc455cb0efe4ed01c2e0b10531
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-06-23 12:23:36 +0200

    typo

--- get_plugin.sh
+++ get_plugin.sh
@@ -4,10 +4,10 @@ set -e
 [ -f common.sh ] || { >&2 echo 'Script must be run from environs root folder'; exit 2; }
 
 for plugin in "$@" ; do
-  if [ $(find plugin/$plugin -maxdepth 1 -type d -empty | wc -l) -eq 0 ] ; then
-    (cd _plugin/$plugin && git pull)
-  else
+  if [ $(find plugin/$plugin -maxdepth 0 -type d -empty | wc -l) -eq 0 ] ; then
     git clone --depth=1 https://github.com/AndriiNikitin/mariadb-environs-$plugin _plugin/$plugin
+  else
+    (cd _plugin/$plugin && git pull)
   fi
 done
 
