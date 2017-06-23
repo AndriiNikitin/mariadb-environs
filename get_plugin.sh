3daccfbd9f86e0c832d5679440cf6ec66f43644b
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-06-23 11:40:59 +0200

    make sure that user data in plugin folders is not deleted

--- get_plugin.sh
+++ get_plugin.sh
@@ -4,7 +4,10 @@ set -e
 [ -f common.sh ] && { >&2 echo 'Script must be run from environs ruoot folder'; exit 2; }
 
 for plugin in "$@" ; do
-  rm -r _plugin/$plugin
-  git clone --depth=1 https://github.com/AndriiNikitin/mariadb-environs-$plugin _plugin/$plugin
+  if [ $(find plugin/$plugin -maxdepth 1 -type d -empty | wc -l) -eq 0 ] ; then
+    (cd _plugin/$plugin && git pull)
+  else
+    git clone --depth=1 https://github.com/AndriiNikitin/mariadb-environs-$plugin _plugin/$plugin
+  fi
 done
 
