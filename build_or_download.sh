85203126a4897d779cafecac45a991a75e1e5956
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-05 12:18:38 +0200

    typo

--- build_or_download.sh
+++ build_or_download.sh
@@ -2,7 +2,7 @@
 set -e
 [ -z "$1" ] && exit 1
 
-if [ "$(find . -maxdepth 1 -type d -name "m$wid*" | wc -l)" -ne 1 ] ; then
+if [ "$(find . -maxdepth 1 -type d -name "$1*" | wc -l)" -ne 1 ] ; then
   >&2 echo "one and only one folder must exist with prefix $1"
   exit 2
 fi
