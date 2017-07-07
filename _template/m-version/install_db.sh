cbef8667165a3c7be86f7430ca87c8c4d803f668
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-07 20:39:21 +0200

    adding test database

--- _template/m-version/install_db.sh
+++ _template/m-version/install_db.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+set -e
 bash __workdir/../_depot/m-tar/__version/scripts/mysql_install_db --defaults-file=__workdir/my.cnf --user=$(whoami) --basedir=__workdir/../_depot/m-tar/__version --force
+mkdir -p __workdir/dt/test
