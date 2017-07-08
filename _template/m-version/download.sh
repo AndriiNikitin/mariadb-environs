c580c47741f97cb8a9c517d4e4ec62c746855d86
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-09 00:33:19 +0200

    wget is too verbose in docker / travis

--- _template/m-version/download.sh
+++ _template/m-version/download.sh
@@ -4,10 +4,36 @@
 
 # example url ftp://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-__version/bintar-linux-x86_64/mariadb-__version-linux-x86_64.tar.gz
 
-FILE=ftp://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-__version/bintar-linux-x86_64/mariadb-__version-linux-x86_64.tar.gz
+file=ftp://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-__version/bintar-linux-x86_64/mariadb-__version-linux-x86_64.tar.gz
 
 mkdir -p __workdir/../_depot/m-tar/__version
 
+(
 cd __workdir/../_depot/m-tar/__version
-[[ -f $(basename $FILE) ]] || { wget -nc $FILE && tar -zxf $(basename $FILE) ${ERN_M_TAR_EXTRA_FLAGS} --strip 1; }
-cd __workdir/..
+
+function cleanup {
+  [ -z "$wgetpid" ] || kill "$wgetpid" 2>/dev/null
+}
+
+trap cleanup INT TERM
+
+if [ ! -f "$(basename $file)"  ] ; then
+  echo downloading "$file"
+  wget -nc $file &
+  wgetpid=$!
+  while kill -0 $wgetpid 2>/dev/null ; do
+    sleep 10
+    echo -n .
+  done
+  wait $wgetpid
+  wgetpid=""
+fi
+
+if [ -f "$(basename $file)" ] ; then
+  if [ ! -x bin/mysqld ] ; then
+    tar -zxf "$(basename $file)" ${ERN_M_TAR_EXTRA_FLAGS} --strip 1
+  fi
+fi
+)
+:
+
