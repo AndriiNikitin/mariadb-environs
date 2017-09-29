27e42009a5b3199db69fc1eac8d66a3a5681ba7d
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-09-29 11:03:08 +0200

    add alternative download mirror

--- _template/m-version/download.sh
+++ _template/m-version/download.sh
@@ -6,6 +6,8 @@
 
 file=ftp://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-__version/bintar-linux-x86_64/mariadb-__version-linux-x86_64.tar.gz
 
+file1=http://mirrors.dotsrc.org/mariadb/mariadb-__version/bintar-linux-x86_64/mariadb-__version-linux-x86_64.tar.gz
+
 mkdir -p __workdir/../_depot/m-tar/__version
 
 (
@@ -19,7 +21,7 @@ trap cleanup INT TERM
 
 if [ ! -f "$(basename $file)"  ] ; then
   echo downloading "$file"
-  wget -q -np -nc $file &
+  ( wget -q -np -nc "$file" || wget -q -np -nc "$file1" )&
   wgetpid=$!
   while kill -0 $wgetpid 2>/dev/null ; do
     sleep 10
@@ -29,7 +31,7 @@ if [ ! -f "$(basename $file)"  ] ; then
   res=$?
   wgetpid=""
   if [ "$res" -ne 0 ] ; then
-    >&2 echo "failed to download '$file' ($res)"
+    >&2 echo "failed to download '$file1' ($res)"
     exit $res 
   fi
 fi
