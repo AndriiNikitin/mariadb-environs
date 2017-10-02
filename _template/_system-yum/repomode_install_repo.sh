b6496f4ebaee8590535637a665843bc3b1c272ce
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-02 17:11:13 +0200

    correction on package naming

--- _template/_system-yum/repomode_install_repo.sh
+++ _template/_system-yum/repomode_install_repo.sh
@@ -5,8 +5,7 @@ set -e
 # yum --showduplicates list MariaDB-server | expand
 # yum -y install MariaDB-server MariaDB-client 
 
-M7VER=$1
-M7MAJOR=${M7VER%\.*}
+M7MAJOR=$1
 if [ "$3" == galera ] ; then
   [ -z "$4" ] || extra=MariaDB-$4
   if [ "$M7MAJOR" == 5.5 ] || [ "$M7MAJOR" == 10.0 ] ; then
