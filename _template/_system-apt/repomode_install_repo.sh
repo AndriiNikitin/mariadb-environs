482a1e1d9ac4d60fe4af8d7a1798c0719264169c
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-02 16:22:03 +0200

    exclude galera from install commands in 10.1+

--- _template/_system-apt/repomode_install_repo.sh
+++ _template/_system-apt/repomode_install_repo.sh
@@ -8,6 +8,15 @@ export DEBIAN_FRONTEND=noninteractive
 debconf-set-selections <<< 'mariadb-server-$M7MAJOR mysql-server/root_password password '"$M7ROOTPWD"
 debconf-set-selections <<< 'mariadb-server-$M7MAJOR mysql-server/root_password_again password '"$M7ROOTPWD"
 
+set -e
+
 apt-get install -y mariadb-server-$M7MAJOR mariadb-client-$M7MAJOR mariadb-server-core-$M7MAJOR
 
-[ -z $3 ] || apt-get install -y mariadb-$3-$M7MAJOR
+if [ ! -z $3 ] ; then
+  if [ "$3" != galera ] || [ "$M7MAJOR" == 5.5 ] ||  [ "$M7MAJOR" == 10.0 ] ; then
+    apt-get install -y mariadb-$3-$M7MAJOR
+  else
+    :
+  fi
+fi
+:
--- _template/_system-yum/repomode_install_repo.sh
+++ _template/_system-yum/repomode_install_repo.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+set -e
 # # TODO upgrade may fail without this - troubleshoot
 # yum --showduplicates list MariaDB-server | expand
 
-yum -y install MariaDB-server MariaDB-client
+yum -y install MariaDB-server MariaDB-client 
 
-[ -z $3 ] || yum -y install MariaDB-$3
+if [ ! -z $3 ] ; then
+  M7VER=$1
+  M7MAJOR=${M7VER%\.*}
+  if [ "$3" != galera ] || [ "$M7MAJOR" == 5.5 ] || [ "$M7MAJOR" == 10.0 ] ) ; then
+    yum -y install MariaDB-$3
+  else
+    :  # do nothing 10.1+ have galera already
+  fi
+fi
